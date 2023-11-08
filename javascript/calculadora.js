1b7107a42103bd61400378ca5a75de4e0f572a58
Santiago Romeu <32142567+santiagoromeu@users.noreply.github.com>
2023-11-08 12:25:10 -0300

    Adding JSON

--- javascript/calculadora.js
+++ javascript/calculadora.js
@@ -1,34 +1,35 @@
 //calculadora de estadia
 
-let total;
-let continua;
-let personas;
-let dias;
-let valres;
-let reservadpt;
+let total=0;
+let resConf;
 const dpts = [
-    {id:1, precio:100, reservado: false, fecha:new Date()},
-    {id:2, precio:110, reservado: false, fecha:new Date()},
-    {id:3, precio:140, reservado: false, fecha:new Date()}
+    {id:1, precio:100, reservado: false},
+    {id:2, precio:110, reservado: false},
+    {id:3, precio:140, reservado: false}
+    ];
+let reserva =[
+    localStorage.getItem("nombre", Reservas.nombre),
+    localStorage.getItem("apellido", Reservas.apellido),
+    localStorage.getItem("email", Reservas.email),
+    localStorage.getItem("huespedes", Reservas.host),
+    localStorage.getItem("dias", Reservas.cant),
+    localStorage.getItem("costo", total),
+    localStorage.getItem("departamento", resConf)
     ];
 
-function Reservas(reserva) {
-    this.nombre = reserva.nombre;
-    this.apellido = reserva.apellido;
-    this.email = reserva.email;
-    this.msg = function(){alert('Hola '+this.nombre+' '+this.apellido+
-    ' en instantes le enviaremos un email para finalizar la reserva, Gracias!')}
-}
+const reservaJSON = JSON.stringify(reserva);
+console.log(reserva)
+console.log(reservaJSON)
 
 function saveForm(){
-    localStorage.setItem("nombre", Reservas.nombre.value)
-    localStorage.setItem("apellido", Reservas.apellido.value)
-    localStorage.setItem("email", Reservas.email.value)
-    localStorage.setItem("huespedes", Reservas.host.value)
-    localStorage.setItem("dias", Reservas.cant.value)
-    localStorage.setItem("departamento", dpts.reservado.value)
-}
-    
+    localStorage.setItem("nombre", Reservas.nombre)
+    localStorage.setItem("apellido", Reservas.apellido)
+    localStorage.setItem("email", Reservas.email)
+    localStorage.setItem("huespedes", Reservas.host)
+    localStorage.setItem("dias", Reservas.cant)
+    localStorage.setItem("costo", total)
+    localStorage.setItem("departamento", resConf)
+}    
 
 function Reservas(reserva) {
     this.nombre = reserva.nombre;
@@ -38,15 +39,18 @@ function Reservas(reserva) {
     this.cant = reserva.cant;
 }
 
-function calcularEstadia(totalCompra){
-    let total=totalCompra * Reservas.cant;
+function calcularEstadia(totalCosto){
+    total=totalCosto * Reservas.cant;
     console.log("El costo total de su estadia sera de U$D"+ total);
     alert("El costo total de su estadia sera de U$D"+total); 
     }
 
+
 let boton = document.getElementById("boton");
 
-boton.onclick = function(){
+boton.addEventListener("click", guardarRes);
+
+function guardarRes(){
     Reservas.nombre = document.getElementById("nombre").value;
     Reservas.apellido = document.getElementById("apellido").value;
     Reservas.email = document.getElementById("email").value;
@@ -54,26 +58,26 @@ boton.onclick = function(){
     Reservas.cant = document.getElementById("cant").value;
     alert("Hola "+ Reservas.nombre + ' ' + Reservas.apellido)
     
-    //isNaN(reserva.cant) && console.log("Ingrese el numero de huespedes y días");
-
     if(Reservas.host<=2){
         alert("Departamento para 1 o 2 personas"); 
         calcularEstadia(dpts[0].precio);
             dpts[0].reservado = confirm('Desea realizar la reserva?');
-            console.log(dpts[0].reservado)
-            alert(dpts[0].reservado)
+            resConf=dpts[0].reservado;
+                saveForm();
     }else if(Reservas.host<=4){
         alert("Departamento para 3 o 4 personas");
         calcularEstadia(dpts[1].precio);
             dpts[1].reservado = confirm('Desea realizar la reserva?');
-            console.log(dpts[1].reservado)
-            alert(dpts[1].reservado)
+            resConf=dpts[1].reservado;
+                saveForm();
     }else if(Reservas.host<=6){
         alert("Departamento para 5 o 6 personas");
         calcularEstadia(dpts[2].precio);
             dpts[2].reservado = confirm('Desea realizar la reserva?');
-            console.log(dpts[2].reservado)
-            alert(dpts[2].reservado)
+            resConf=dpts[2].reservado;
+                saveForm();
     }else{
         alert("No contamos el departamento adecuado");
-    }}
+    }
+}
+
